b504a1e0670610f1a590eb9f434d548e8cd3bbce
Joban Mejia <js.mejiar12@uniandes.edu.co>
2024-09-20 08:38:40 -0500

    Entitys y repos

---
+++ src/main/java/co/edu/uniandes/dse/parcialprueba/entities/EspecialidadEntity.java
@@ -0,0 +1,30 @@
+package co.edu.uniandes.dse.parcialprueba.entities;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import jakarta.persistence.Entity;
+
+import jakarta.persistence.ManyToMany;
+
+import lombok.Data;
+import uk.co.jemos.podam.common.PodamExclude;
+
+
+
+@Data
+@Entity
+
+public class EspecialidadEntity extends BaseEntity{
+
+	private String name;
+	private String descripcion;
+
+    @PodamExclude
+    @ManyToMany
+    private List<MedicoEntity> medico = new ArrayList<>();
+
+
+}
